891d02351cbc18da667f0bb6692076ef03d82d2b
David Huerta <dhuerta23@gmail.com>
2015-08-23 12:23:05 +0200

    footer height value to 10

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 				console.log("h: %d, w: %d",h,w);
 				//alert("resize");
 				
-				$('footer').css('top',h+685);
+				$('footer').css('top',h+10);
 
 				if(w<=1285 && w>=810){
 					$('footer').css('height','80px');
