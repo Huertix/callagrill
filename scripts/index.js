717e8e0d012382a015234e1456bf743f415f70b0
David Huerta <dhuerta23@gmail.com>
2015-08-24 10:40:00 +0200

    metiendo fotos a service

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,3 @@
-var h = 0;
-var w = 0;
-
-var footerAdjust = function(){
-      var win = $(this); //this = window
-      if (win.height() >= 820) { /* ... */ }
-      if (win.width() >= 1280) { /* ... */ }
-      
-      h = $('body').height();
-      w = $('body').width();
-      
-		console.log(" body h: %d, w: %d",h,w);
-		//alert("resize");
-		
-		
-
-		if(w<=1190){
-			$('footer').css('height','80px');
-			console.log('1 '+w);
-		}else{
-			$('footer').css('height','40px');
-			console.log('2 '+w);
-		}
-		
-}
-
-var waitForFinalEvent = (function () {
-	  var timers = {};
-	  return function (callback, ms, uniqueId) {
-	    if (!uniqueId) {
-	      uniqueId = "Don't call this twice without a uniqueId";
-	    }
-	    if (timers[uniqueId]) {
-	      clearTimeout (timers[uniqueId]);
-	    }
-	    timers[uniqueId] = setTimeout(callback, ms);
-	  };
-	})();
-
-
-
 function cliked(){	
 
 	$("a").each(function() {
@@ -53,7 +12,6 @@ function cliked(){
 	    $(this).addClass("active");
 	});
 }
-
 
 
 $(document).ready(function(){
@@ -72,24 +30,13 @@ $(document).ready(function(){
 	     $('article').load($(this).attr('href'));
 	});	
 
-	waitForFinalEvent(function(){
-			footerAdjust();
-	});
-
-
-
-
 });
 
 
 
-$(window).resize(function(){
-	waitForFinalEvent(function(){
-		footerAdjust();
-		console.log("resize");	
-	});
-	
-});
+
+
+
 
 
  
